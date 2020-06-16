d078d2c0136b342b5a84d846b097cfb1708e95bc
Milos Misurovic <milos.misurovic@synergysuite.com>
2020-06-16 16:05:37 +0200

    Quick fix.

--- src/vineyard_transactions/core.clj
+++ src/vineyard_transactions/core.clj
@@ -33,7 +33,7 @@
 
 (defn -main
   [& port-number]
-  (let [port (if (nil? port-number) (System/getenv "PORT") port-number)]
+  (let [port (if (nil? port-number) (first (System/getenv "PORT")) port-number)]
     (do
      (db/create-tables)
      (log/info (str "Start server on port: " port)))
